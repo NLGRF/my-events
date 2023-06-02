2ca824c4b21957fad26be907e6744e7c8de6dcbe
Nonpavit Detbun <non.devtech@gmail.com>
2023-06-02 21:48:05 +0700

    poc

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,32 +1,87 @@
-import React from "react";
+import React, { useState, useEffect, useCallback }  from "react";
 import ReactDOM from "react-dom";
 
 const Modal = ({ onClose, children, title }) => {
-    const handleCloseClick = (e) => {
-        e.preventDefault();
-        onClose();
-    };
+  // message data from the event
+  const [msgData, setMsgData] = useState();
+  // connect to the event
+  // const eventSource = new EventSource('http://localhost:3001/sse');
+  const eventSource = new EventSource('http://localhost:3001/stocksFind');
 
-    const modalContent = (
-        <div className="modal-overlay">
-            <div className="modal-wrapper">
-                <div className="modal">
-                    <div className="modal-header">
-                        <a href="#" onClick={handleCloseClick}>
-                            x
-                        </a>
-                    </div>
-                    {title && <h1>{title}</h1>}
-                    <div className="modal-body">{children}</div>
-                </div>
-            </div>
-        </div>
-    );
+  eventSource.onmessage = ({data}) => {
+    // console.log('New message', JSON.parse(data));
+    // setMsgData(data)
+    // if (JSON.parse(data).symbol === 'Stock #2' && JSON.parse(data).bid === '100') {
+    //   setMsgData(data)
+    // }
+    // loop through the data and check if the symbol is Stock #2 and the bid is 100
+    JSON.parse(data).forEach((stock) => {
+      // console.log('stock', stock)
+      if (stock.symbol === 'Stock #2' && stock.bid === 100) {
+        setMsgData(data)
+      }
+    }
+    )
+  };
 
-    return ReactDOM.createPortal(
-        modalContent,
-        document.getElementById("modal-root")
-    );
+  // create ref for the StyledModalWrapper component
+  const modalWrapperRef = React.useRef();
+
+  // check if the user has clicked inside or outside the modal
+  // useCallback is used to store the function reference, so that on modal closure, the correct callback can be cleaned in window.removeEventListener
+  const backDropHandler = useCallback(e => {
+      if (!modalWrapperRef?.current?.contains(e.target)) {
+          onClose();
+      }
+  }, []);
+
+  // if have a message, log it to the console and close the modal
+  useEffect(() => {
+    if (msgData) {
+      // console.log('New message', JSON.parse(msgData));
+      onClose();
+    }
+  }, [msgData]);
+
+  useEffect(() => {
+      // We wrap it inside setTimeout in order to prevent the eventListener to be attached before the modal is open.
+      setTimeout(() => {
+          window.addEventListener('click', backDropHandler);
+      })
+  }, [])
+
+  useEffect(() => {
+      // remove the event listener when the modal is closed
+      return () => window.removeEventListener('click', backDropHandler);
+  }, []);
+
+  const handleCloseClick = (e) => {
+      e.preventDefault();
+      onClose();
+  };
+
+  const modalContent = (
+      <div className="modal-overlay">
+          {/* Wrap the whole Modal inside the newly created StyledModalWrapper
+          and use the ref */}
+          <div ref={modalWrapperRef} className="modal-wrapper">
+              <div className="modal">
+                  <div className="modal-header">
+                      <a href="#" onClick={handleCloseClick}>
+                          x
+                      </a>
+                  </div>
+                  {title && <h1>{title}</h1>}
+                  <div className="modal-body">{children}</div>
+              </div>
+          </div>
+      </div>
+  );
+
+  return ReactDOM.createPortal(
+      modalContent,
+      document.getElementById("modal-root")
+  );
 };
 
 export default Modal
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,64 @@
 import Modal from "src/components/Modal";
-import {useState} from "react";
-
+import { useState } from "react";
+import { useQRCode } from "next-qrcode";
 
 export default function Home() {
   const [showModal, setShowModal] = useState(false);
+  const [showQRCode, setShowQRCode] = useState(false);
+  const { Canvas } = useQRCode();
 
+  // newDeposit with post request
+  const createStock = async () => {
+    console.log("createStock");
+    const res = await fetch("http://localhost:3001/stocks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        symbol: "Stock #2",
+        ask: 789,
+        bid: 100,
+      }),
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+  
+  const generateQRCode = () => {
+    createStock();
+    console.log("generateQRCode");
+    setShowQRCode(true);
+  };
   return (
-      <div>
-        <button onClick={() => setShowModal(true)}>Open Modal</button>
-        {showModal &&
-            <Modal onClose={() => setShowModal(false)}>
-                Hello from the modal!
-            </Modal>
-        }
-      </div>
-  )
+    <div>
+      <button onClick={() => setShowModal(true)}>Open Modal</button>
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <h1>Hello from the modal!</h1>
+          <button onClick={() => generateQRCode()}>new Stock</button>
+          <hr />
+          {showQRCode && (
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              {/* <Canvas text="https://metamask.app.link/send/0x200C234721b5e549c3693CCc93cF191f90dC2aF9/transfer?address=0x65b7CE9C408A8E9b0Bc37d3024B8ec43544A226c&uint256=3e18" /> */}
+              {/* <Canvas text="http://localhost:3001/stocks" /> */}
+              <Canvas
+                text="https://37c4-49-0-84-39.ngrok-free.app/stocks"
+                options={{
+                  level: "M",
+                  margin: 2,
+                  scale: 5,
+                  width: 450,
+                  // color: {
+                  //   dark: "#252525",
+                  //   light: "#c8b370",
+                  // },
+                }}
+              />
+            </div>
+          )}
+        </Modal>
+      )}
+    </div>
+  );
 }
