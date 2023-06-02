d7979d0c90b8bb6f17836ab1c9c65f04d88d3e9f
Nonpavit Detbun <non.devtech@gmail.com>
2023-06-02 13:14:18 +0700

    modal v1

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import Modal from "src/components/Modal";
+import {useState} from "react";
+
+
+export default function Home() {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+      <div>
+        <button onClick={() => setShowModal(true)}>Open Modal</button>
+        {showModal &&
+            <Modal onClose={() => setShowModal(false)}>
+                Hello from the modal!
+            </Modal>
+        }
+      </div>
+  )
+}
